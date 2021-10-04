2f540090acdeae1b59d4099bc0a647de5db54120
JustPlay-Code <64895799+JustPlay-Code@users.noreply.github.com>
2021-10-04 19:48:20 +0200

    commit

--- Moto parabolico autocompiler/Index.ino
+++ Moto parabolico autocompiler/Index.ino
@@ -1,10 +1,10 @@
 void setup {
 //  Insert data without their unit of mesure (9,81 instead of 9,81m/s)
-    int Sx;
-    int Sy;
-    int V;
+    int Sx = ;
+    int Sy = ;
+    int V = ;
     int a = 9,81;
-    int t;
+    int t = ;
     int z = 0; };
 
 int main {
@@ -22,7 +22,8 @@ int main {
         z++ };
 
 if (z<2){
-    cout << "Not enough data, please retry." << endl;
+    Serial.print("/!\ Not enough data, please retry /!\ ");
+    Serial.print("\n");
     exit (abort-not_enough_data);
     else z = 0 };
 
@@ -41,16 +42,23 @@ do {
 //  Break the rule!  
     z++;
     if (z > 5) {
+    Serial.print("/!\ Not enough data, please retry /!\ ");
+    Serial.print("\n");
         exit (abort-to_much_calc)};
     
 } while (Sx != /, Sy != /, V != /, t != /,);
 //  output
-    cout << "La distanza dal punto di partenza (Sx) è di " Sx "m" <<endl;
-    cout << "L'altezza (Sy) è di " Sy "m" <<endl;
-    cout << "La velocità impiegata (V) è di " V "m/s" <<endl;
-    cout << "Il tempo impiegato per toccare terra (t) è di " t "s"  <<endl;
-    cout << "L'accellerazione di gravità (g/a) è di " a "m/s" <<endl;
-    
-    exit (code_success)};
+    Serial.print("Output:");
+    Serial.print("\n\n");
+    Serial.print("La distanza dal punto di partenza (Sx) è di " Sx "m");
+    Serial.print("\n");
+    Serial.print("L'altezza (Sy) è di " Sy "m");
+    Serial.print("\n");
+    Serial.print("La velocità impiegata (V) è di " V "m/s");
+    Serial.print("\n");
+    Serial.print("Il tempo impiegato per toccare terra (t) è di " t "s");
+    Serial.print("\n");
+    Serial.print("L'accellerazione di gravità (g, a) è di " a "m/s");
+    Serial.print("\n");
 
     void loop() {}
